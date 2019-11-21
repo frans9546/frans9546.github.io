cb2de2720ad524e4a4f03af1cb06cbd45b8aeabc
franskristian <franskristian76@gmail.com>
2019-11-21 13:34:45 +0700

    Manjuah juah

---
+++ precache-manifest.6f545486f533dadc63b22a9e7c33254c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acb1fea038ed7ea06680",
+    "url": "/css/chunk-vendors.450cd38a.css"
+  },
+  {
+    "revision": "eef4d8a3db4c2c99b059630c94d21805",
+    "url": "/index.html"
+  },
+  {
+    "revision": "980093d83fe22b1c2b29",
+    "url": "/js/app.a15869be.js"
+  },
+  {
+    "revision": "acb1fea038ed7ea06680",
+    "url": "/js/chunk-vendors.936b366c.js"
+  },
+  {
+    "revision": "c6ca81b3e3abc047a004e9a69b050dfb",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
